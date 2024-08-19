795cefc9768b0df5673bb8df3937113d85f25518
whitneyyuen <whitneyuen@yahoo.com.hk>
2024-08-19 01:05:29 -0400

    155. Min Stack Solved Medium Topics Companies Hint Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
    Implement the MinStack class:
    
    MinStack() initializes the stack object.
    void push(int val) pushes the element val onto the stack.
    void pop() removes the element on the top of the stack.
    int top() gets the top element of the stack.
    int getMin() retrieves the minimum element in the stack.
    You must implement a solution with O(1) time complexity for each function.
    
    Example 1:
    
    Input
    ["MinStack","push","push","push","getMin","pop","top","getMin"]
    [[],[-2],[0],[-3],[],[],[],[]]
    
    Output
    [null,null,null,null,-3,null,0,-2]
    
    Explanation
    MinStack minStack = new MinStack();
    minStack.push(-2);
    minStack.push(0);
    minStack.push(-3);
    minStack.getMin(); // return -3
    minStack.pop();
    minStack.top();    // return 0
    minStack.getMin(); // return -2
    
    Constraints:
    
    -231 <= val <= 231 - 1
    Methods pop, top and getMin operations will always be called on non-empty stacks.
    At most 3 * 104 calls will be made to push, pop, top, and getMin.

---
+++ 155. Min Stack.java
@@ -0,0 +1,38 @@
+class MinStack {
+    Deque<Integer> stack;
+
+    public MinStack() {
+        stack = new ArrayDeque<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        int min = stack.element();
+        for (int s: stack) {
+            if (s<min) {
+                min = s;
+            }
+        }
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
