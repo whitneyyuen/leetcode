c464945012bfbb26047a17131d4e5741ff4124f4
whitneyyuen <whitneyuen@yahoo.com.hk>
2024-11-15 16:33:59 -0500

    15. 3Sum Medium Topics Companies Hint Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
    Notice that the solution set must not contain duplicate triplets.
    
    Example 1:
    
    Input: nums = [-1,0,1,2,-1,-4]
    Output: [[-1,-1,2],[-1,0,1]]
    Explanation:
    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
    The distinct triplets are [-1,0,1] and [-1,-1,2].
    Notice that the order of the output and the order of the triplets does not matter.
    Example 2:
    
    Input: nums = [0,1,1]
    Output: []
    Explanation: The only possible triplet does not sum up to 0.
    Example 3:
    
    Input: nums = [0,0,0]
    Output: [[0,0,0]]
    Explanation: The only possible triplet sums up to 0.
    
    Constraints:
    
    3 <= nums.length <= 3000
    -105 <= nums[i] <= 105

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -1,30 +1,35 @@
 class Solution {
-	public List<List<Integer>> threeSum(int[] nums) {
-		Arrays.sort(nums);
-		List<List<Integer>> resultList = new ArrayList<List<Integer>>();
-		for (int i=0;i<nums.length-2;i++) {
-			for (int j=i+1;j<nums.length-1;j++) {
-				for (int k=j+1;k<nums.length;k++) {
-					if (nums[i]+nums[j]+nums[k]==0) {
-						List<Integer> tempList = new ArrayList<Integer>();
-						tempList.add(nums[i]);
-						tempList.add(nums[j]);
-						tempList.add(nums[k]);
-						boolean containInd = false;
-						for (List<Integer> l: resultList) {
-							if (l.get(0)==tempList.get(0) &&
-								l.get(1)==tempList.get(1) &&
-								l.get(2)==tempList.get(2)) {
-								containInd = true;
-							}
-						}
-						if (!containInd) {
-							resultList.add(tempList);
-						}
-					}
-				}
-			}
-		}
-		return resultList;
-	}
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> resultList = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip the same element to avoid duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    resultList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Move left and right to the next different numbers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return resultList;
+    }
 }
